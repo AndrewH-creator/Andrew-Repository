e92410e1d66e47bb7d318beb2cf701d33a52e2be
AndrewH-creator <hissongas@gmail.com>
2026-09-17 11:45:22 -0400

    Add UPC check digit function

--- Lab1_AHissong-1.py
+++ Lab1_AHissong-1.py
@@ -7,3 +7,21 @@ Purpose: This program validates a 12-digit UPC-A code by calculating
 Starter Code: None. This program was written from the assignment requirements.
 Date: September 17, 2026
 """
+def find_UPC(upc_first_11):
+    """
+    Calculates and returns the expected UPC-A check digit.
+    """
+
+    total = 0
+
+    for i in range(11):
+        digit = int(upc_first_11[i])
+
+        if i % 2 == 0:
+            total += digit * 3
+        else:
+            total += digit
+
+    check_digit = (10 - (total % 10)) % 10
+
+    return check_digit
