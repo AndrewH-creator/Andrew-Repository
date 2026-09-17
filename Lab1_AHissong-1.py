13c74cf1de0a9bdc65e0755b093121d13fd7a253
AndrewH-creator <hissongas@gmail.com>
2026-09-17 11:44:35 -0400

    Create UPC validator file and header

---
+++ Lab1_AHissong-1.py
@@ -0,0 +1,9 @@
+"""
+Program Name: Lab 1 - UPC Validator
+Author: Andrew Hissong
+Purpose: This program validates a 12-digit UPC-A code by calculating
+         the expected check digit and comparing it to the check digit
+         provided by the user.
+Starter Code: None. This program was written from the assignment requirements.
+Date: September 17, 2026
+"""
